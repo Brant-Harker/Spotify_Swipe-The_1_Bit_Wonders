6f1742e876c3a9372a4c23cb74008863aee9f839
Brant-Harker <brant.harker12@gmail.com>
2023-01-07 15:04:18 -0700

    First parameters

--- backend/authorization.py
+++ backend/authorization.py
@@ -1,3 +1,14 @@
 """
 This module authorizes access to the Spotify API using Authorization Flow + PKCE
 """
+from hashlib import sha256
+
+# Parameters
+client_id     = "980aa9bf0ae54f888ba9fb79bf1ba8b9"
+response_type = "code"
+redirect_uri  = "https://localhost"
+state         = "secure string"
+scope         = "image-upload user-library-modify playlist-modify-private playlist-modify-public streaming"
+
+# PKCE Parameters
+code_challenge_method = "S256"
