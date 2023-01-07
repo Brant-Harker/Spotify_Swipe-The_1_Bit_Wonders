b3653adfb0faec87a8134d1900c06c73ce87c1c6
Oliver <77119741+OliverHalasan@users.noreply.github.com>
2023-01-07 14:21:26 -0700

    place holder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="">
+      <header className="d-flex align-items-center flex-column">
+        <div className='d-flex flex-row justify-content-center'>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h3>
+            1 Bit Wonders
+          </h3>
+        </div>
+        <div className='border w-75'>
+          <div className='d-flex justify-content-center '>
+            <h3>Discovery Weekly</h3>
+          </div>
+          <div className='d-flex justify-content-center'>
+          <img src="placeholder.jpg" className='w-75' />
 
+          </div>
+          
+        </div>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
