cff52872c342656f657606e983569f642ac5a397
Carlos Marquez <73247260+carlosm3@users.noreply.github.com>
2023-01-07 13:37:32 -0700

    Add metadata module

---
+++ backend/metadata.py
@@ -0,0 +1,38 @@
+""" 
+    This module requests metadata from the Spotify API and returns it to the frontend in JSON format.
+"""
+
+# Song metadata class
+class Song:
+    def __init__(self, name, artist, album, preview_url, image_url):
+        self.name = name
+        self.artist = artist
+        self.album = album
+        self.preview_url = preview_url
+        self.image_url = image_url
+
+    def __str__(self):
+        return f"{self.name} by {self.artist} on {self.album}"
+
+    def __repr__(self):
+        return f"{self.name} by {self.artist} on {self.album}"
+
+    def to_json(self):
+        return {
+            "name": self.name,
+            "artist": self.artist,
+            "album": self.album,
+            "preview_url": self.preview_url,
+            "image_url": self.image_url
+        }
+
+
+if __name__ == "__main__":
+    song = Song("Song Name", "Artist Name", "Album Name", "Preview URL", "Image URL")
+    print(song)
+    print(song.to_json())
+
+# Artist
+# Artist logo
+# Album cover
+# Spotify logo
