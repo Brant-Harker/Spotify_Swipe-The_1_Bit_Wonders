8ee61577b128195f5d7ed75588aeda7ab0f4d6da
Oliver <77119741+OliverHalasan@users.noreply.github.com>
2023-01-07 22:18:55 -0700

    bug fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  
 );
 
 // If you want your app to work offline and load faster, you can change
@@ -20,4 +20,3 @@ serviceWorkerRegistration.unregister();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
