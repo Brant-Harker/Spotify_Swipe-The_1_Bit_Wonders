0247a1940e761ad6b011b65456274c6d441218b0
Oliver <77119741+OliverHalasan@users.noreply.github.com>
2023-01-07 21:27:52 -0700

    swipe cards

--- frontend/src/componets/Album.js
+++ frontend/src/componets/Album.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Music from "./Music";
 
 
@@ -10,8 +10,9 @@ export default function Album({ src }) {
           <h3 className="text-success mt-3 mb-4">Album Title</h3>
         </div>
         <div className='d-flex align-items-center flex-column'>
-          <img src="placeholder.jpg" className='w-100' />
+          <img src="placeholder.jpg" className='w-100' alt="placeholder" />
           <Music />
+          
         </div>
 </>
 
--- frontend/src/componets/PlayButton.js
+++ frontend/src/componets/PlayButton.js
@@ -13,21 +13,6 @@ import FastRewindRounded from '@mui/icons-material/FastRewindRounded';
 
 
 
-const Widget = styled('div')(({ theme }) => ({
-    padding: 16,
-    borderRadius: 16,
-    width: 343,
-    maxWidth: '100%',
-    margin: 'auto',
-    position: 'relative',
-    zIndex: 1,
-    backgroundColor:
-        theme.palette.mode === 'dark' ? 'rgba(0,0,0,0.6)' : 'rgba(255,255,255,0.4)',
-    backdropFilter: 'blur(40px)',
-}));
-
-
-
 const TinyText = styled(Typography)({
     fontSize: '0.75rem',
     opacity: 0.38,
